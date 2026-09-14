aad57fb69f345a1c3b6c7b90a628eeef7fe8d01a
Caua Parente <cauahenriqueccpar@gmail.com>
2026-09-14 17:17:05 -0300

    Primeiro Commit

---
+++ ExerciciosAlura1.py
@@ -0,0 +1,19 @@
+print("Python na Escola de Programacao da Alura\n")
+
+nome = "Cauã"
+
+idade = 21
+
+print(f"Meu nome {nome} é e tenho {idade} anos")
+
+print("""
+A
+L
+U
+R
+A
+""")
+
+pi = 3.14159
+
+print(f"O Valor arredondado de pi é {pi:.2f}")
